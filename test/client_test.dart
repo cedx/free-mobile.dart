7f250cff86af4a8ed6434a5f8cb68f0ed6066ea9
Cédric Belin <cedric@belin.io>
2016-12-21 18:02:34 +0100

    Added the skeleton of the `Client` unit tests

---
+++ test/client_test.dart
@@ -0,0 +1,42 @@
+import 'package:test/test.dart';
+import '../lib/freemobile.dart';
+
+/// Tests the features of the `Client` class.
+void main() {
+
+  /// Tests the constructor.
+  group('Client', () {
+
+    test('TODO', () {
+
+    });
+
+    test('TODO', () {
+
+    });
+  });
+
+  /// Tests the `sendMessage()` method.
+  group('.sendMessage', () {
+
+    test('TODO', () {
+
+    });
+
+    test('TODO', () {
+
+    });
+  });
+
+  /// Tests the `toJson()` method.
+  group('.toJson', () {
+
+    test('TODO', () {
+
+    });
+
+    test('TODO', () {
+
+    });
+  });
+}
