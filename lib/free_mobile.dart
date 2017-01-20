1c8d6f0bc02a49ffa06bdf215d6cdc808cc123e3
Cédric Belin <cedric@belin.io>
2017-01-20 01:01:27 +0100

    Added some requirements

--- lib/free_mobile.dart
+++ lib/free_mobile.dart
@@ -5,9 +5,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:math' as math;
 import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 part 'src/client.dart';
 
 /// Sends a SMS message to a given Free Mobile account, and returns the response body.
-Future<String> sendMessage(String text, {String username, String password}) async
+Future<String> sendMessage(String text, {@required String username, @required String password}) async
   => new Client(username, password).sendMessage(text);
