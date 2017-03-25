0413870cd3269f651cc5542b3551b03accc7cccc
Cédric Belin <cedric@belin.io>
2017-03-25 02:56:27 +0100

    Removed the `sendMessage` function

--- lib/free_mobile.dart
+++ lib/free_mobile.dart
@@ -5,10 +5,5 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:math' as math;
 import 'package:http/http.dart' as http;
-import 'package:meta/meta.dart';
 
 part 'src/client.dart';
-
-/// Sends a SMS message to a given Free Mobile account, and returns the response body.
-Future<String> sendMessage(String text, {@required String username, @required String password}) async
-  => new Client(username, password).sendMessage(text);
