7b7312ada231b9318fe519776024dda3bede2787
Cédric Belin <cedric@belin.io>
2019-04-17 20:17:31 +0200

    CI: added support for browser testing

--- tool/grind.dart
+++ tool/grind.dart
@@ -15,7 +15,10 @@ void clean() {
 }
 
 @Task('Uploads the results of the code coverage')
-void coverage() => Pub.run('coveralls', arguments: const ['var/lcov.info']);
+void coverage() {
+  final report = getFile('var/lcov.info');
+  if (report.existsSync()) Pub.run('coveralls', arguments: [report.path]);
+}
 
 @Task('Builds the documentation')
 Future<void> doc() async {
@@ -33,20 +36,10 @@ void lint() => Analyzer.analyze(existingSourceDirs);
 
 @Task('Runs the test suites')
 Future<void> test() async {
-  await Pub.runAsync('coverage', script: 'collect_coverage', arguments: [
-    '--out=var/coverage.json',
-    '--resume-isolates',
-    '--uri=${await _profileTest(getFile('test/all.dart'))}',
-    '--wait-paused'
-  ]);
-
-  return Pub.runAsync('coverage', script: 'format_coverage', arguments: [
-    '--in=var/coverage.json',
-    '--lcov',
-    '--out=var/lcov.info',
-    '--packages=.packages',
-    '--report-on=${libDir.path}'
-  ]);
+  final args = context.invocation.arguments;
+  return (args.hasOption('platform') ? args.getOption('platform') : 'vm') == 'browser'
+    ? Pub.runAsync('build_runner', arguments: ['test', '--delete-conflicting-outputs', '--', '--platform=firefox'])
+    : _collectCoverage(getFile('test/all.dart'));
 }
 
 @Task('Upgrades the project to the latest revision')
@@ -59,6 +52,24 @@ void upgrade() {
 
 @Task('Watches for file changes')
 void watch() => Pub.run('build_runner', arguments: ['watch', '--delete-conflicting-outputs']);
+
+/// Collects the code coverage by running the specified test file.
+Future<void> _collectCoverage(File testFile) async {
+  await Pub.runAsync('coverage', script: 'collect_coverage', arguments: [
+    '--out=var/coverage.json',
+    '--resume-isolates',
+    '--uri=${await _profileTest(testFile)}',
+    '--wait-paused'
+  ]);
+
+  return Pub.runAsync('coverage', script: 'format_coverage', arguments: [
+    '--in=var/coverage.json',
+    '--lcov',
+    '--out=var/lcov.info',
+    '--packages=.packages',
+    '--report-on=${libDir.path}'
+  ]);
+}
 
 /// Profiles the execution of the specified test file.
 /// Returns the URI that Observatory is listening on.
